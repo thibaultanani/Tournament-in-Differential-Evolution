2fcbbcb43302c76c7691d2bb92a275af7b195b53
thibaultanani <thuny.ta@gmail.com>
2024-01-03 22:04:00 +0100

    Update to include setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ml-feature-selection',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'mrmr - selection',
+        'numpy',
+        'openpyxl',
+        'pandas',
+        'scikit - learn',
+        'scipy',
+    ],
+    author='Thibault Anani',
+    author_email='thuny.ta@gmail.com',
+    description='Implements multiple type of filter methods and heuristics for the feature selection problem in '
+                'machine learning as well as a new one: tournament in differential evolution',
+    url='https://github.com/thibaultanani/Tournament-in-Differential-Evolution',
+)
